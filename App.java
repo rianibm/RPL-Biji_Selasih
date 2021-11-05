2516ceb8e9d2d1b77477912550a4437161b0b01c
Julfasinta <90094748+Julfasinta@users.noreply.github.com>
2021-11-05 08:13:28 +0700

    Add files via upload

--- App.java
+++ App.java
@@ -3,8 +3,8 @@ package id.ac.ukdw.fti.rpl.kelompokbijiselasih;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene; //scene itu konten di windownya
-import javafx.stage.Stage; //stage sbg window baru
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -13,26 +13,26 @@ import java.io.IOException;
  */
 public class App extends Application {
 
- private static Scene scene; //scene belum ada nilai, diset di bawah
+ private static Scene scene;
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("main")); //di dalam start dibuat scene baru dengan parameter FXMLoader
+        scene = new Scene(loadFXML("main"));
         stage.setScene(scene);
         stage.show();
     }
 
     static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml)); //set root agar bisa diakses pop up window
+        scene.setRoot(loadFXML(fxml));
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml")); //diambil nama filenya apa
-        return fxmlLoader.load(); //diload file nya
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
-        launch(); //launch fungsi bawaan dari application
+        launch();
     }
 
 }
