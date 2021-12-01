83339ad399f82b0f27a46ba843f00a599aa6840e
Julfasinta <olivia.julfasinta@ti.ukdw.ac.id>
2021-12-01 13:46:35 +0700

    commit olip

--- Iterasi 2/tugas-akhir-rpl/tugas-akhir-rpl/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module id.ac.ukdw.fti.rpl.kelompokbijiselasih {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-    requires java.sql;
-    
-    opens id.ac.ukdw.fti.rpl.kelompokbijiselasih to javafx.fxml;
-    exports id.ac.ukdw.fti.rpl.kelompokbijiselasih;
-}
